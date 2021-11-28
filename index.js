08960b21c32b796faaf60537df8aff95ad596abd
Moe Deera <deeramoe@gmail.com>
2021-11-28 16:23:52 -0700

    commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+
+const app = express();
+
+
+app.get('/', (req, res) => {
+
+
+res.send('its working')
+
+})
+
+
+if (process.env.NODE_ENV === 'production'){
+
+    app.use(express.static('client/build'))
+    app.get('*', (req,res)=>{
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    
+    })
+}
+
+
+
+const PORT = process.env.PORT || 7700;
+
+
+app.listen(PORT, console.log('its running'));
