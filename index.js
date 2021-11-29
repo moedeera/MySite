a4a9777174680b40c4fc220ce48395f74f991661
Moe Deera <deeramoe@gmail.com>
2021-11-28 18:56:01 -0700

    commit

--- index.js
+++ index.js
@@ -11,14 +11,14 @@ res.send('its working')
 })
 
 
-// if (process.env.NODE_ENV === 'production'){
+if (process.env.NODE_ENV === 'production'){
 
-//     app.use(express.static('client/build'))
-//     app.get('*', (req,res)=>{
-//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    app.use(express.static('client/build'))
+    app.get('*', (req,res)=>{
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
     
-//     })
-// }
+    })
+}
 
 
 
