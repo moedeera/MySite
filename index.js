206619a73e1754abac58fa7cf68867e117028739
moedeera <82620264+moedeera@users.noreply.github.com>
2021-12-01 01:31:06 -0600

    commit

--- index.js
+++ index.js
@@ -19,7 +19,8 @@ if (process.env.NODE_ENV === 'production'){
 
     app.use(express.static('client/build'))
     app.get('*', (req,res)=>{
-    res.sendFile(path.resolve(__dirname, 'client', 'public', 'index.html'))
+        const index = path.join(__dirname, 'build', 'index.html');
+        res.sendFile(index);
     
     })
 }
